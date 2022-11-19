0ac6831b8990274114a983147308ff10ed3399f8
YQmQY <2418406011@qq.com>
2022-11-19 18:03:07 +0800

    second commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * @Author: YQmQY
+ * @Date: ${DATE}
+ * @Version: 1.0.0
+ * @Describe: TODO
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("hello git");
+        System.out.println("hello git commit second");
+    }
+}
